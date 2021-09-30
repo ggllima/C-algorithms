3f0e599a66950a020394cd30c8a3992a37e52d51
ggllima <ggllima@usp.br>
2021-09-29 22:38:19 -0300

    in test

--- crivoErastostenes/crivoErastostenes.c
+++ crivoErastostenes/crivoErastostenes.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#define max_numbers 10000
+
+#define RANGE 10000
 
 int main() {
 
   
-  int prime_numbers[max_numbers], list_prime[max_numbers], quantity_of_numbers;
-  int* indices;
+  int prime_numbers[RANGE], list_prime[RANGE], quantity_of_numbers;
+  int* indices, number, count = 0;
 
+  // input da quantidade de primos que serão printados
   scanf("%d",&quantity_of_numbers);
 
+  // alocando dinamicamente a quantidade de indices que serão acessados
   indices = malloc(quantity_of_numbers * sizeof(int));
 
+  // input dos indices da lista de primos
   for (int i = 0; i < quantity_of_numbers; i++)
     scanf("%d", &indices[i]);
 
-  int indicator = 0;
-  for (int index=1; index<=max_numbers; index++) {
+  
+  for (int index=1; index<=RANGE; index++) {
     prime_numbers[index] = index;
   }
 
-  int i = 2;
-  while ((i*i) <= max_numbers)
+  
+  
+  for(number = 2; (number * number) <= RANGE; number++)
   {
-      if (prime_numbers[i] != 0)
+      // verifica se o numero é múltiplo de 2 para não colocar na lista
+      if (prime_numbers[number] != 0)
       {
-          for(int j=2; j<max_numbers; j++)
+          for(int indice=2; indice<RANGE; indice++)
           {
-              if (prime_numbers[i]*j > max_numbers)
+              // verifica se o numero irá estourar o RANGE de 10000
+              if (prime_numbers[number]*indice > RANGE)
                   break;
               else
               
-                  prime_numbers[prime_numbers[i]*j]=0;
+                  prime_numbers[prime_numbers[number]*indice]=0;
           }
       }
-      i++;
+  
   }
-  int count = 0;
-  for(int index = 1; index<=max_numbers; index++)
+  
+  // irá colocar todos os numeros primos dentro do RANGE na lista list_primes
+  for(int index = 1; index<=RANGE; index++)
   {
    
       if (prime_numbers[index]!=0){
@@ -47,6 +54,7 @@ int main() {
       }
   }
 
+  // irá printar somentes os indices que foram inputados
   for (int index = 0; index < quantity_of_numbers; index++)
   {
     printf("%d ",list_prime[indices[index]]);
